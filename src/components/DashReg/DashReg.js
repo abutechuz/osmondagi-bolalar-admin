1f891d7c934bbe9b8cf4f512dbb310d97e07ff3f
abdulqodir18 <ukkosha45@gmail.com>
2021-04-23 18:19:04 +0500

    super app

--- src/components/DashReg/DashReg.js
+++ src/components/DashReg/DashReg.js
@@ -18,11 +18,11 @@ function DashReg() {
 
   function stil (arg) {
     if (Number(arg) < 25) {
-      return ["rgba(41, 204, 150, 0.8)", "rgba(41, 204, 150, 0.2)"]
+      return ["rgba(254, 0, 0, 0.8)", "rgba(254, 0, 0, 0.2)"]
     } else if (Number(arg) > 25 && Number(arg) < 50) {
       return ["rgba(254, 195, 0, 0.8)", "rgba(254, 195, 0, 0.2)"]
     } else {
-      return ["rgba(254, 0, 0, 0.8)", "rgba(254, 0, 0, 0.2)"]
+      return ["rgba(41, 204, 150, 0.8)", "rgba(41, 204, 150, 0.2)"]
     }
   }
 
