e35f59b2b4b1bc20b1d8fce69b049a35bf6ef8d7
abdulqodir18 <ukkosha45@gmail.com>
2021-04-23 17:00:24 +0500

    local

--- src/components/DashAge/DashAge.js
+++ src/components/DashAge/DashAge.js
@@ -1,39 +1,25 @@
 import "./DashAge.scss"
+import { useEffect } from "react";
+import { useAsync } from "../../hooks/useAsync";
+import { client } from "../../utils/api-client";
 
 function DashAge () {
+
+  const { data, isLoading, isError, run } = useAsync();
+
+  useEffect(() => {
+    run(
+      client("age").then((data) => {
+        console.log(data);
+      })
+    );
+  }, [run]);
+
   return (
     <>
       <div className="stats__age stats-age">
         <h3 className="stats-age__title">Age</h3>
         <ul className="stats-age__list">
-          <li className="stats-age__item">
-            <div className="stats-age__inner">
-              <p className="stats-age__text">0 - 16 yosh</p>
-              <span className="stats-age__count">70%</span>
-            </div>
-            <span className="stats-age__dio" aria-label="70%"></span>
-          </li>
-          <li className="stats-age__item">
-            <div className="stats-age__inner">
-              <p className="stats-age__text">0 - 16 yosh</p>
-              <span className="stats-age__count">70%</span>
-            </div>
-            <span className="stats-age__dio" aria-label="70%"></span>
-          </li>
-          <li className="stats-age__item">
-            <div className="stats-age__inner">
-              <p className="stats-age__text">0 - 16 yosh</p>
-              <span className="stats-age__count">70%</span>
-            </div>
-            <span className="stats-age__dio" aria-label="70%"></span>
-          </li>
-          <li className="stats-age__item">
-            <div className="stats-age__inner">
-              <p className="stats-age__text">0 - 16 yosh</p>
-              <span className="stats-age__count">70%</span>
-            </div>
-            <span className="stats-age__dio" aria-label="70%"></span>
-          </li>
           <li className="stats-age__item">
             <div className="stats-age__inner">
               <p className="stats-age__text">0 - 16 yosh</p>
--- src/components/DashProf/DashProf.js
+++ src/components/DashProf/DashProf.js
@@ -1,16 +1,29 @@
 import "./DashProf.scss";
 import "../DashReg/DashReg.scss"
+import { useEffect } from "react";
+import { useAsync } from "../../hooks/useAsync";
+import { client } from "../../utils/api-client";
 
 function DashProf() {
+
+  const { data, isLoading, isError, run } = useAsync();
+
+  useEffect(() => {
+    run(
+      client("profession").then((data) => {
+        console.log(data);
+      })
+    );
+  }, [run]);
+
   return (
     <>
       <div className="stats__prof stats-prof">
         <h3 className="stats-prof__title">Profassion</h3>
-
         <ul className="stats-prof__list">
-          <li className="stats-reg__item">
-            <p className="stats-reg__text">Tashkent</p>
-            <span className="stats-reg__span">10%</span>
+          <li className="stats-prof__item">
+            <p className="stats-prof__text">It tadbirkor</p>
+            <span className="stats-prof__span">10%</span>
           </li>
         </ul>
       </div>
