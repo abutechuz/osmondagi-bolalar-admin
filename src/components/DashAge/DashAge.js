81de947a1ac84b6d36904400e2b612fe4d85ac46
ilhomjon55 <ilhomshamidinov55@gmail.com>
2021-04-24 00:00:49 +0500

    Add

--- src/components/DashAge/DashAge.js
+++ src/components/DashAge/DashAge.js
@@ -1,33 +1,38 @@
-import "./DashAge.scss";
-import { useEffect } from "react";
-import { useAsync } from "../../hooks/useAsync";
-import { client } from "../../utils/api-client";
+import './DashAge.scss'
+import { useEffect } from 'react'
+import { useAsync } from '../../hooks/useAsync'
+import { client } from '../../utils/api-client'
 
 function DashAge() {
-  const { data, isLoading, isError, run } = useAsync();
+    const { data, isLoading, isError, run } = useAsync()
 
-  useEffect(() => {
-    run(client("age"));
-  }, [run]);
+    useEffect(() => {
+        run(client('age'))
+    }, [run])
 
-  return (
-    <>
-      <div className="stats__age stats-age">
-        <h3 className="stats-age__title">Age</h3>
-        <ul className="stats-age__list">
-          {data?.map((e, i) => (
-            <li className="stats-age__item" key={i}>
-              <div className="stats-age__inner">
-                <p className="stats-age__text">{e.desc}</p>
-                <span className="stats-age__count">{e.percent}%</span>
-              </div>
-              <span className="stats-age__dio" aria-label="70%" style={{width: e.percent + '%'}}></span>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className='stats__age stats-age'>
+                <h3 className='stats-age__title'>Age</h3>
+                <ul className='stats-age__list'>
+                    {data?.map((e, i) => (
+                        <li className='stats-age__item' key={i}>
+                            <div className='stats-age__inner'>
+                                <p className='stats-age__text'>{e.desc}</p>
+                                <span className='stats-age__count'>
+                                    {e.percent}%
+                                </span>
+                            </div>
+                            <span
+                                className='stats-age__dio'
+                                aria-label='70%'
+                                style={{ width: e.percent + '%' }}></span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
 }
 
-export default DashAge;
+export default DashAge
--- src/components/DashProf/DashProf.js
+++ src/components/DashProf/DashProf.js
@@ -1,31 +1,33 @@
-import "./DashProf.scss";
-import "../DashReg/DashReg.scss";
-import { useEffect } from "react";
-import { useAsync } from "../../hooks/useAsync";
-import { client } from "../../utils/api-client";
+import './DashProf.scss'
+import '../DashReg/DashReg.scss'
+import { useEffect } from 'react'
+import { useAsync } from '../../hooks/useAsync'
+import { client } from '../../utils/api-client'
 
 function DashProf() {
-  const { data, isLoading, isError, run } = useAsync();
+    const { data, isLoading, isError, run } = useAsync()
 
-  useEffect(() => {
-    run(client("profession"));
-  }, [run]);
+    useEffect(() => {
+        run(client('profession'))
+    }, [run])
 
-  return (
-    <>
-      <div className="stats__prof stats-prof">
-        <h3 className="stats-prof__title">Profassion</h3>
-        <ul className="stats-prof__list">
-          {data?.map((e, i) => (
-            <li className="stats-prof__item" key={Math.random()}>
-              <p className="stats-prof__text">{ e.text }</p>
-              <span className="stats-prof__span">{e.percent}%</span>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className='stats__prof stats-prof'>
+                <h3 className='stats-prof__title'>Profession</h3>
+                <ul className='stats-prof__list'>
+                    {data?.map((e, i) => (
+                        <li className='stats-prof__item' key={Math.random()}>
+                            <p className='stats-prof__text'>{e.text}</p>
+                            <span className='stats-prof__span'>
+                                {e.percent}%
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
 }
 
-export default DashProf;
+export default DashProf
