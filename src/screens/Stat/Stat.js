894b8f165356892080b21008ea401ab86dba943a
abdulqodir18 <ukkosha45@gmail.com>
2021-04-23 15:24:52 +0500

    change header

--- src/screens/Stat/Stat.js
+++ src/screens/Stat/Stat.js
@@ -2,17 +2,19 @@ import './Stat.scss'
 import Container from '../../components/Container/Container'
 import DashReg from '../../components/DashReg/DashReg'
 import DashAge from '../../components/DashAge/DashAge'
+import DashProf from '../../components/DashProf/DashProf'
 
 function Stat() {
     return (
         <>
             <section className='stats'>
                 <h2 className='visually-hidden'>section</h2>
-                <Container>
+                <Container className="stats__container">
                     <div className='stats__inner'>
                         <DashReg />
                         <DashAge />
                     </div>
+                    <DashProf />
                 </Container>
             </section>
         </>
