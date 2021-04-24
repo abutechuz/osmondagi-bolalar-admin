4f4584efe8468c7ceb6d2ed003f11f17160f5f80
ilhomjon55 <ilhomshamidinov55@gmail.com>
2021-04-24 14:13:46 +0500

    Complete Speaker Table

--- src/components/SpeakerTable/SpeakerTable.js
+++ src/components/SpeakerTable/SpeakerTable.js
@@ -5,66 +5,81 @@ import { useAsync } from '../../hooks/useAsync'
 import { client } from '../../utils/api-client'
 
 function SpeakerTable() {
+    const [page, setPage] = React.useState(1)
+
+    const [elements, setElements] = React.useState(0)
+
+    const elList = React.useRef(null)
+
     const { data, isSuccess, run } = useAsync()
 
     React.useEffect(() => {
-        run(client('speakers'))
-    }, [run])
+        run(client(`speakers?limit=5&page=${page}`))
+    }, [run, page])
 
+    React.useEffect(() => {
+        setElements(elList?.current?.childElementCount)
+    }, [data])
     return (
         <div className='speaker-table__wrapper'>
             <h2 className='speaker-section__heading title'>
                 Barcha Speaker uchun Takliflar
             </h2>
             <table className='speaker-table'>
-                <thead className='speaker-table'>
-                    <tr className='speaker-table__item'>
-                        <th className='speaker-table__right-box'>ID</th>
-                        <th className='speaker-table__right-box'>
-                            Speaker ismi
+                <thead className='speaker-table__head'>
+                    <tr className='speaker-table__head-tr'>
+                        <th className='speaker-table__head-th speaker-table__head-th--id'>
+                            ID
                         </th>
-                        <th className='speaker-table__right-box'>
+                        <th className='speaker-table__head-th speaker-table__head-th-name'>Speaker ismi</th>
+                        <th className='speaker-table__head-th speaker-table__head-th-surname'>
                             Speaker familiyasi
                         </th>
-                        <th className='speaker-table__right-box'>Kasbi</th>
-                        <th className='speaker-table__right-box'>Yoshi</th>
-                        <th className='speaker-table__right-box'>Ish joyi</th>
-                        <th className='speaker-table__right-box'>
+                        <th className='speaker-table__head-th speaker-table__head-th-profession'>Kasbi</th>
+                        <th className='speaker-table__head-th speaker-table__head-th-age'>Yoshi</th>
+                        <th className='speaker-table__head-th speaker-table__head-th-work'>Ish joyi</th>
+                        <th className='speaker-table__head-th speaker-table__head-th-time'>
                             Tavsiya etilgan vaqt
                         </th>
                     </tr>
                 </thead>
-                <tbody className='speaker-table__body'>
+                <tbody className='speaker-table__body' ref={elList}>
                     {isSuccess &&
                         data?.map((speaker) => (
-                            <tr className='speaker-table__item'>
-                                <td className='speaker-table__right-box'>
-                                    {speaker.id ?? 'kiritilmagan'}
+                            <tr
+                                className='speaker-table__body'
+                                key={speaker.id + Math.random()}>
+                                <td className='speaker-table__body-td speaker-table__body-td--id'>
+                                    {speaker.id ?? '-'}
                                 </td>
-                                <td className='speaker-table__right-box'>
-                                    {speaker.name ?? 'kiritilmagan'}
+                                <td className='speaker-table__body-td speaker-table__body-td-name'>
+                                    {speaker.name ?? '-'}
                                 </td>
-                                <td className='speaker-table__right-box'>
-                                    {speaker.surname ?? 'kiritilmagan'}
+                                <td className='speaker-table__body-td speaker-table__body-td-surname'>
+                                    {speaker.surname ?? '-'}
                                 </td>
-                                <td className='speaker-table__right-box'>
-                                    {speaker.profession ?? 'kiritilmagan'}
+                                <td className='speaker-table__body-td speaker-table__body-td-profession'>
+                                    {speaker.profession ?? '-'}
                                 </td>
-                                <td className='speaker-table__right-box'>
-                                    {speaker.age ?? 'kiritilmagan'}
+                                <td className='speaker-table__body-td speaker-table__body-td-age'>
+                                    {speaker.age ?? '-'}
                                 </td>
-                                <td className='speaker-table__right-box'>
-                                    {speaker.workplace ?? 'kiritilmagan'}
+                                <td className='speaker-table__body-td speaker-table__body-td-work'>
+                                    {speaker.workplace ?? '-'}
                                 </td>
-                                <td className='speaker-table__right-box'>
-                                    {speaker.time ?? 'kiritilmagan'}
+                                <td className='speaker-table__body-td speaker-table__body-td-time'>
+                                    {speaker.time ?? '-'}
                                 </td>
                             </tr>
                         ))}
                 </tbody>
             </table>
 
-            <TableController />
+            <TableController
+                setPage={setPage}
+                page={page}
+                numberLi={elements}
+            />
         </div>
     )
 }
