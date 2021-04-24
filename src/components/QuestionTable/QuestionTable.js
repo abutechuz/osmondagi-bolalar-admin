2c030a621e1470f165f10a2bffc4f501f03f0618
ilhomjon55 <ilhomshamidinov55@gmail.com>
2021-04-24 10:38:24 +0500

    Add questions

--- src/components/QuestionTable/QuestionTable.js
+++ src/components/QuestionTable/QuestionTable.js
@@ -5,89 +5,32 @@ import { useAsync } from '../../hooks/useAsync'
 import { client } from '../../utils/api-client'
 
 function QuestionTable() {
-    const { data, isLoading, isError, run } = useAsync()
+    const { data, isSuccess, run } = useAsync()
 
     React.useEffect(() => {
-        run(client('advices').then((data) => console.log(data)))
+        run(client('questions'))
     }, [run])
 
     return (
         <div className='question-list__wrapper'>
             <h2 className='question-section__heading'>Barcha Savollar</h2>
             <ul className='question-list'>
-                <li className='question-list__item'>
-                    <div className='question-list__right-box'>
-                        <h3 className='question-list__username'>AbduQodir</h3>
-                        <small className='question-list__date'>
-                            24.05.2019
-                        </small>
-                    </div>
-                    <div className='question-list__question'>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Inventore fugiat possimus tempora rem natus
-                        assumenda alias illum eveniet nam dicta! Lorem ipsum
-                        dolor, sit amet consectetur adipisicing elit. Inventore
-                        fugiat possimus tempora rem natus assumenda alias illum
-                        eveniet nam dicta! Lorem ipsum dolor, sit amet
-                        consectetur adipisicing elit. Inventore fugiat possimus
-                        tempora rem natus assumenda alias illum eveniet nam
-                        dicta! Lorem ipsum dolor, sit amet consectetur
-                        adipisicing elit. Inventore fugiat possimus tempora rem
-                        natus assumenda alias illum eveniet nam dicta!
-                    </div>
-                </li>
-                <li className='question-list__item'>
-                    <div className='question-list__right-box'>
-                        <h3 className='question-list__username'>AbduQodir</h3>
-                        <small className='question-list__date'>
-                            24.05.2019
-                        </small>
-                    </div>
-                    <div className='question-list__question'>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Inventore fugiat possimus tempora rem natus
-                        assumenda alias illum eveniet nam dicta!
-                    </div>
-                </li>
-                <li className='question-list__item'>
-                    <div className='question-list__right-box'>
-                        <h3 className='question-list__username'>AbduQodir</h3>
-                        <small className='question-list__date'>
-                            24.05.2019
-                        </small>
-                    </div>
-                    <div className='question-list__question'>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Inventore fugiat possimus tempora rem natus
-                        assumenda alias illum eveniet nam dicta!
-                    </div>
-                </li>
-                <li className='question-list__item'>
-                    <div className='question-list__right-box'>
-                        <h3 className='question-list__username'>AbduQodir</h3>
-                        <small className='question-list__date'>
-                            24.05.2019
-                        </small>
-                    </div>
-                    <div className='question-list__question'>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Inventore fugiat possimus tempora rem natus
-                        assumenda alias illum eveniet nam dicta!
-                    </div>
-                </li>
-                <li className='question-list__item'>
-                    <div className='question-list__right-box'>
-                        <h3 className='question-list__username'>AbduQodir</h3>
-                        <small className='question-list__date'>
-                            24.05.2019
-                        </small>
-                    </div>
-                    <div className='question-list__question'>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Inventore fugiat possimus tempora rem natus
-                        assumenda alias illum eveniet nam dicta!
-                    </div>
-                </li>
+                {isSuccess &&
+                    data?.map((q) => (
+                        <li className='question-list__item' key={q?.time}>
+                            <div className='question-list__right-box'>
+                                <h3 className='question-list__username'>
+                                    {q?.username}
+                                </h3>
+                                <small className='question-list__date'>
+                                    {q?.time}
+                                </small>
+                            </div>
+                            <div className='question-list__question'>
+                                {q?.question}
+                            </div>
+                        </li>
+                    ))}
             </ul>
 
             <TableController />
