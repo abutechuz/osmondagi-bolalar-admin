2712e7f4565f26d4c1a10310a22b1bd61ba12c94
abdulqodir18 <ukkosha45@gmail.com>
2021-04-25 15:19:08 +0500

    dash post statr

--- src/components/DashPost/DashPost.js
+++ src/components/DashPost/DashPost.js
@@ -1,24 +1,13 @@
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import {useState } from 'react'
 import "./DashPost.scss"
+import { useState, useEffect } from 'react';
 
 function DashPosts () {
-  const [x, setX]  = useState()
-  return (
-    <>
-        <CKEditor
-          editor={ ClassicEditor }
-          data="<p>Hello from CKEditor 5!</p>"
-          onChange={ (e, editor) => {
-              // You can store the "editor" and use when it is needed.
-              setX(editor.getData())
-              console.log(editor.getData())
-          }}
-          />
-        <div dangerouslySetInnerHTML={{ __html: x }}></div>
-    </>
-  )
-}
-
-export default DashPosts
+    
+    return (
+      <>
+      <h2>sdsdsd</h2>
+      </>
+    )
+  }
+  
+  export default DashPosts
