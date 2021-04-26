31ce2b038245e0e3eb0a3f3d383b4dea7bd14ff3
ilhomjon55 <ilhomshamidinov55@gmail.com>
2021-04-27 02:44:30 +0500

    Complete

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
 import './App.scss'
-import { Switch, Route } from 'react-router-dom'
-import DashNav from './components/DashNav/DashNav'
-import DashMain from './components/DashMain/DashMain'
-import Stat from './screens/Stat/Stat'
-import Question from './screens/Question/Question'
-import Offer from './screens/Offer/Offer'
-import Speaker from './screens/Speaker/Speaker'
-import Posts from './screens/Posts/Posts'
-import User from './screens/User/User'
-import DashChart from './screens/DashChart/DashChart'
-import Login from './screens/Login/Login'
+import AuthenticatedApp from './AuthenticatedApp.js'
+import UnAuthenticatedApp from './UnAuthenticatedApp.js'
+import useToken from './hooks/useToken'
 
 function App() {
-    return (
-        <>
-            <div className='site-main'>
-                <DashNav />
-                <div className='site-content'>
-                    <DashMain />
-                    <Switch>
-                        <Route path='/' component={Stat} exact />
-                        <Route path='/chart-stat' component={DashChart} />
-                        <Route path='/question' component={Question} exact />
-                        <Route path='/offer' component={Offer} exact />
-                        <Route path='/speaker' component={Speaker} exact />
-                        <Route path='/posts' component={Posts} exact />
-                        <Route path='/users' component={User} exact />
-                        <Route path='/login' component={Login} exact />
-                    </Switch>
-                </div>
-            </div>
-        </>
-    )
+    const [token] = useToken()
+    if (token) {
+        return <AuthenticatedApp />
+    } else {
+        return <UnAuthenticatedApp />
+    }
 }
 
 export default App
