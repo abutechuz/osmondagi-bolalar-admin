32ef3a09305e63ac28db6363ff07328f495eea2e
ilhomjon55 <ilhomshamidinov55@gmail.com>
2021-05-01 14:59:32 +0500

    Complete nested link

--- src/components/WebNav/WebNav.js
+++ src/components/WebNav/WebNav.js
@@ -1,41 +1,57 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import './WebNav.scss'
 
 function WebNav() {
     return (
-        <ul className='webdash__nav'>
-            <li className='webdash__nav-link'>
-                <NavLink to='/webdash' exact>
+        <ul className='webdash__nav-list'>
+            <li className='webdash__nav-item'>
+                <NavLink className='webdash__nav-link' to='/webdash' exact>
                     Orders
                 </NavLink>
             </li>
-            <li className='webdash__nav-link'>
-                <NavLink to='/webdash/videos' exact>
+            <li className='webdash__nav-item'>
+                <NavLink
+                    className='webdash__nav-link'
+                    to='/webdash/videos'
+                    exact>
                     Videos
                 </NavLink>
             </li>
-            <li className='webdash__nav-link'>
-                <NavLink to='/webdash/team' exact>
+            <li className='webdash__nav-item'>
+                <NavLink className='webdash__nav-link' to='/webdash/team' exact>
                     Team
                 </NavLink>
             </li>
-            <li className='webdash__nav-link'>
-                <NavLink to='/webdash/categories' exact>
+            <li className='webdash__nav-item'>
+                <NavLink
+                    className='webdash__nav-link'
+                    to='/webdash/categories'
+                    exact>
                     Categories
                 </NavLink>
             </li>
-            <li className='webdash__nav-link'>
-                <NavLink to='/webdash/products' exact>
+            <li className='webdash__nav-item'>
+                <NavLink
+                    className='webdash__nav-link'
+                    to='/webdash/products'
+                    exact>
                     Products
                 </NavLink>
             </li>
-            <li className='webdash__nav-link'>
-                <NavLink to='/webdash/offers' exact>
+            <li className='webdash__nav-item'>
+                <NavLink
+                    className='webdash__nav-link'
+                    to='/webdash/offers'
+                    exact>
                     Offers
                 </NavLink>
             </li>
-            <li className='webdash__nav-link'>
-                <NavLink to='/webdash/stores' exact>
+            <li className='webdash__nav-item'>
+                <NavLink
+                    className='webdash__nav-link'
+                    to='/webdash/stores'
+                    exact>
                     Stores
                 </NavLink>
             </li>
--- src/screens/WebDash/WebDash.js
+++ src/screens/WebDash/WebDash.js
@@ -9,20 +9,32 @@ import Products from '../../components/Products/Products'
 import Offers from '../../components/Offers/Offers.js'
 import Stores from '../../components/Stores/Stores.js'
 import Orders from '../../components/Orders/Orders.js'
+import './WebDash.scss'
 
 function WebDash() {
     return (
-        <div>
+        <div className='webdash-wrapper'>
+            <div className='webdash-content'>
+                <Switch>
+                    <Route path='/webdash' component={Orders} exact />
+                    <Route path='/webdash/videos' component={Videos} exact />
+                    <Route path='/webdash/team' component={Team} exact />
+                    <Route
+                        path='/webdash/categories'
+                        component={Categories}
+                        exact
+                    />
+                    <Route
+                        path='/webdash/products'
+                        component={Products}
+                        exact
+                    />
+                    <Route path='/webdash/offers' component={Offers} exact />
+                    <Route path='/webdash/stores' component={Stores} exact />
+                </Switch>
+            </div>
+
             <WebNav />
-            <Switch>
-                <Route path='/webdash' component={Orders}  exact/>
-                <Route path='/webdash/videos' component={Videos} exact />
-                <Route path='/webdash/team' component={Team} exact />
-                <Route path='/webdash/categories' component={Categories} exact />
-                <Route path='/webdash/products' component={Products} exact />
-                <Route path='/webdash/offers' component={Offers} exact />
-                <Route path='/webdash/stores' component={Stores} exact />
-            </Switch>
         </div>
     )
 }
