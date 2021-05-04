38549199d3e466a79c69eaf23bb50df891461799
ilhomjon55 <ilhomshamidinov55@gmail.com>
2021-05-04 14:12:11 +0500

    Add pagination

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,45 +1,87 @@
 import React from 'react'
 import Container from '../Container/Container'
 import './orders.scss'
+import { useQuery } from 'react-query'
+import TableController from '../TableContoller/TableController'
 
 function Orders() {
-    const [orders, setOrders] = React.useState()
+    const [page, setPage] = React.useState(1)
 
-    fetch('http://192.168.0.208:5000/orders', {
+    const [elements, setElements] = React.useState(0)
+
+    const elList = React.useRef(null)
+
+    const fetchProjects = (page = 0) =>
+        fetch('http://165.227.211.149:5472/orders?l=5&p=' + page, {
             method: 'GET',
             headers: {
                 token:
                     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMDYkYWxpb3lLRkVVUWd5VzJ2S2MwUXc3dW1vOVZIWjFDa0U1UUFmTkliT0V0Wi9iSUMvRzZLRi4iLCJpYXQiOjE2MjAwMjQzMDZ9.ifbelGdLxQ3Hg_RbtT3RIpAw9V1MGLBpDkrrnh1zh-I',
             },
-        }).then(data => data.json()).then(data => setOrders(data))
+        }).then((data) => data.json())
+
+    const { data, isSuccess } = useQuery(
+        ['orders', page],
+        () => fetchProjects(page),
+        { keepPreviousData: true }
+    )
+
+    React.useEffect(() => {
+        setElements(elList?.current?.childElementCount)
+    }, [data])
 
     return (
-        <section className="orders">
+        <section className='orders'>
             <Container>
-                <div className="orders__wrapper">
-                    <h2 className="orders__heading">Buyurtmalar</h2>
+                <div className='orders__wrapper'>
+                    <h2 className='orders__heading'>Buyurtmalar</h2>
 
-                    <table className="orders__table">
-                        <tr className="orders__table-row">
-                            <th className="orders__table-column">Soni</th>
-                            <th className="orders__table-column">O'lchami</th>
-                            <th className="orders__table-column">To'liq ism</th>
-                            <th className="orders__table-column">Viloyat</th>
-                            <th className="orders__table-column">Telefon raqami</th>
-                            <th className="orders__table-column">To'liq manzili</th>
-                        </tr>
+                    <table className='orders__table'>
+                        <thead>
+                            <tr className='orders__table-row'>
+                                <th className='orders__table-column'>Soni</th>
+                                <th className='orders__table-column'>
+                                    O'lchami
+                                </th>
+                                <th className='orders__table-column'>
+                                    To'liq ism
+                                </th>
+                                <th className='orders__table-column'>
+                                    Viloyat
+                                </th>
+                                <th className='orders__table-column'>
+                                    Telefon raqami
+                                </th>
+                                <th className='orders__table-column'>
+                                    To'liq manzili
+                                </th>
+                            </tr>
+                        </thead>
 
-                        {orders && orders?.map((order) => (
-                        <tr className="orders__table-row">
-                            <td>{order.order_amount ?? '-'}</td>
-                            <td>{order.product_id ?? '-'}</td>
-                            <td>{order.order_fullname ?? '-'}</td>
-                            <td>{order.order_region ?? '-'}</td>
-                            <td>{order.order_phonenumber ?? '-'}</td>
-                            <td>{order.order_address ?? '-'}</td>
-                        </tr>
-                        ))}
+                        <tbody ref={elList}>
+                            {isSuccess &&
+                                data?.map((order) => (
+                                    <tr
+                                        className='orders__table-row'
+                                        key={order.order_id}>
+                                        <td>{order.order_amount ?? '-'}</td>
+                                        <td>{order.product_id ?? '-'}</td>
+                                        <td>{order.order_fullname ?? '-'}</td>
+                                        <td>{order.order_region ?? '-'}</td>
+                                        <td>
+                                            {order.order_phonenumber ?? '-'}
+                                        </td>
+                                        <td>{order.order_address ?? '-'}</td>
+                                    </tr>
+                                ))}
+                        </tbody>
                     </table>
+
+                    <TableController
+                        setPage={setPage}
+                        page={page}
+                        numberLi={elements}
+                    />
                 </div>
             </Container>
         </section>
--- src/components/WebOffers/WebOffers.js
+++ src/components/WebOffers/WebOffers.js
@@ -1,46 +1,81 @@
 import React from 'react'
+import { useQuery } from 'react-query'
+import Container from '../Container/Container'
+import moment from 'moment'
 import './WebOffers.scss'
+import TableController from '../TableContoller/TableController'
 
 function WebOffers() {
+    const [page, setPage] = React.useState(1)
+
+    const [elements, setElements] = React.useState(0)
+
+    const elList = React.useRef(null)
+
+    const fetchProjects = (page = 0) =>
+        fetch('http://165.227.211.149:5472/offers?l=5&p=' + page, {
+            method: 'GET',
+            headers: {
+                token:
+                    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMDYkYWxpb3lLRkVVUWd5VzJ2S2MwUXc3dW1vOVZIWjFDa0U1UUFmTkliT0V0Wi9iSUMvRzZLRi4iLCJpYXQiOjE2MjAwMjQzMDZ9.ifbelGdLxQ3Hg_RbtT3RIpAw9V1MGLBpDkrrnh1zh-I',
+            },
+        }).then((data) => data.json())
+
+    const { data, isSuccess } = useQuery(
+        ['offers', page],
+        () => fetchProjects(page),
+        { keepPreviousData: true }
+    )
+
+    React.useEffect(() => {
+        setElements(elList?.current?.childElementCount)
+    }, [data])
+
     return (
-        <section className='offer-section'>
-            <form className='webdash-offer__form'>
-                <div className='webdas-offer__left'>
-                    <div className='webdash-offer__input-wrapper'>
-                        <label
-                            htmlFor='member-offer_img_link'
-                            className='webdash-offer__label'>
-                            Ism va Familiya
-                        </label>
-                        <input
-                            className='webdash-offer__input'
-                            type='text'
-                            id='member-offer_img_link'
-                            placeholder='Eshmatov Toshmat...'
-                        />
-                    </div>
+        <section className='offers'>
+            <Container>
+                <div className='offers__wrapper'>
+                    <h2 className='offers__heading'>Takliflar</h2>
+
+                    <table className='offers__table'>
+                        <thead>
+                            <tr className='offers__table-row'>
+                                <th className='offers__table-column'>
+                                    To'liq ism
+                                </th>
+                                <th className='offers__table-column'>Sana</th>
+                                <th className='offers__table-column'>Taklif</th>
+                            </tr>
+                        </thead>
+                        <tbody ref={elList}>
+                            {isSuccess &&
+                                data.map((offer) => (
+                                    <tr
+                                        className='offers__table-row'
+                                        key={
+                                            offer.offer_id +
+                                            offer.offer_fullname
+                                        }>
+                                        <td>{offer.offer_fullname}</td>
+                                        <td>
+                                            {moment(offer.offer_sent_at).format(
+                                                'L'
+                                            )}
+                                        </td>
+
+                                        <td>{offer.offer_summary}</td>
+                                    </tr>
+                                ))}
+                        </tbody>
+                    </table>
+
+                    <TableController
+                        setPage={setPage}
+                        page={page}
+                        numberLi={elements}
+                    />
                 </div>
-
-                <div className='webdash-offer__right'>
-                    <div className='webdash-offer__input-wrapper'>
-                        <label
-                            htmlFor='member-offer_summary '
-                            className='webdash-offer__label'>
-                            Taklif 
-                        </label>
-
-                        <textarea
-                            className='webdash-offer__textarea'
-                            type='text'
-                            id='member-offer_summary '
-                            placeholder='Taklif'
-                        >
-                        </textarea>
-                    </div>
-                </div>
-
-                <button className='webdash-offer__submit-btn'>Submit</button>
-            </form>
+            </Container>
         </section>
     )
 }
