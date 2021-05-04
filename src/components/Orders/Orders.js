dfeb5eb62af67492de53a1e5bfb0bf313661fa6f
jakhongirs <j.sagdullaev31@gmail.com>
2021-05-04 11:40:47 +0500

    Almost completed orders page

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -3,6 +3,16 @@ import Container from '../Container/Container'
 import './orders.scss'
 
 function Orders() {
+    const [orders, setOrders] = React.useState()
+
+    fetch('http://192.168.0.208:5000/orders', {
+            method: 'GET',
+            headers: {
+                token:
+                    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMDYkYWxpb3lLRkVVUWd5VzJ2S2MwUXc3dW1vOVZIWjFDa0U1UUFmTkliT0V0Wi9iSUMvRzZLRi4iLCJpYXQiOjE2MjAwMjQzMDZ9.ifbelGdLxQ3Hg_RbtT3RIpAw9V1MGLBpDkrrnh1zh-I',
+            },
+        }).then(data => data.json()).then(data => setOrders(data))
+
     return (
         <section className="orders">
             <Container>
@@ -18,14 +28,17 @@ function Orders() {
                             <th className="orders__table-column">Telefon raqami</th>
                             <th className="orders__table-column">To'liq manzili</th>
                         </tr>
+
+                        {orders && orders?.map((order) => (
                         <tr className="orders__table-row">
-                            <td>2</td>
-                            <td>L</td>
-                            <td>Jakhongir Sagdullaev</td>
-                            <td>Qashqadaryo</td>
-                            <td>+998990171448</td>
-                            <td>Qarshi Shahri, Oq-tepa mahallasi, 1-uy</td>
+                            <td>{order.order_amount ?? '-'}</td>
+                            <td>{order.product_id ?? '-'}</td>
+                            <td>{order.order_fullname ?? '-'}</td>
+                            <td>{order.order_region ?? '-'}</td>
+                            <td>{order.order_phonenumber ?? '-'}</td>
+                            <td>{order.order_address ?? '-'}</td>
                         </tr>
+                        ))}
                     </table>
                 </div>
             </Container>
