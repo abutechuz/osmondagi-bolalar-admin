646a9627a9e84533ee2eb732ee1920a115f886fc
ilhomjon55 <ilhomshamidinov55@gmail.com>
2021-05-04 11:22:32 +0500

    push

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import {useQuery} from 'react-query'
+import { useQuery } from 'react-query'
 import './Team.scss'
-
 
 /* 192.168.0.208:5000
 192.168.0.208:5501 */
 function Team() {
+    const { data, isSuccess } = useQuery({
+        queryKey: 'team',
+
+        queryFn: () =>
+            fetch('http://192.168.0.208:5000/team', {
+                token:
+                    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMDYkYWxpb3lLRkVVUWd5VzJ2S2MwUXc3dW1vOVZIWjFDa0U1UUFmTkliT0V0Wi9iSUMvRzZLRi4iLCJpYXQiOjE2MjAwMjQzMDZ9.ifbelGdLxQ3Hg_RbtT3RIpAw9V1MGLBpDkrrnh1zh-I',
+            }).then((data) => data.json()),
+    })
+
     function handleSubmitTeam(evt) {
         evt.preventDefault()
 
@@ -26,14 +35,6 @@ function Team() {
         formData.append('facebook', member_facebook_link.value.trim())
         formData.append('instagram', member_instagram_link.value.trim())
 
-        console.log(
-            member_fullname.value.trim(),
-            member_status.value.trim(),
-            member_img.files[0],
-            member_telegram_link.value.trim(),
-            member_facebook_link.value.trim(),
-            member_instagram_link.value.trim()
-        )
         fetch('http://192.168.0.208:5000/team', {
             method: 'POST',
             body: formData,
@@ -42,6 +43,7 @@ function Team() {
                     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMDYkYWxpb3lLRkVVUWd5VzJ2S2MwUXc3dW1vOVZIWjFDa0U1UUFmTkliT0V0Wi9iSUMvRzZLRi4iLCJpYXQiOjE2MjAwMjQzMDZ9.ifbelGdLxQ3Hg_RbtT3RIpAw9V1MGLBpDkrrnh1zh-I',
             },
         }).then(() => {
+            alert('Added')
             member_fullname.value = ''
             member_status.value = ''
             member_img.value = null
@@ -51,10 +53,30 @@ function Team() {
         })
     }
 
+    function handleClickDeleteMember(evt) {
+        const formData = new FormData()
+        formData.append('member_id', evt.target.dataset.memberid)
+
+        fetch('http://192.168.0.208:5000/team', {
+            method: 'DELETE',
+            body: formData,
+            headers: {
+                token:
+                    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMDYkYWxpb3lLRkVVUWd5VzJ2S2MwUXc3dW1vOVZIWjFDa0U1UUFmTkliT0V0Wi9iSUMvRzZLRi4iLCJpYXQiOjE2MjAwMjQzMDZ9.ifbelGdLxQ3Hg_RbtT3RIpAw9V1MGLBpDkrrnh1zh-I',
+            },
+        }).then(() => alert('Deleted'))
+    }
+
     return (
         <section className='webdash-team-section'>
-            <form className='webdash-team__form' onSubmit={handleSubmitTeam}>
+            <form
+                className='webdash-team__form'
+                onSubmit={handleSubmitTeam}
+                autoComplete='off'>
                 <div className='webdas-team__left'>
+                    <h2 className='webdash-team__heading title'>
+                        Jamoa A'zosini Qo'shish
+                    </h2>
                     <div className='webdash-team__input-wrapper'>
                         <label
                             htmlFor='member_fullname'
@@ -149,6 +171,28 @@ function Team() {
 
                 <button className='webdash-team__submit-btn'>Submit</button>
             </form>
+
+            <div className='team-members__wrapper'>
+                <h3 className='team-members__heading'>Mavjud Jamoa A'zolari</h3>
+                <ul className='team-members__list'>
+                    {isSuccess &&
+                        data?.map((t) => (
+                            <li
+                                className='team-members__item'
+                                key={t.member_id}>
+                                {t.member_fullname
+                                    ? t.member_fullname
+                                    : 'kiritilmagan'}
+                                <button
+                                    className='team-members__item-btn'
+                                    data-memberid={t.member_id}
+                                    onClick={handleClickDeleteMember}>
+                                    Delete
+                                </button>
+                            </li>
+                        ))}
+                </ul>
+            </div>
         </section>
     )
 }
