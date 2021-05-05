1547a72db86f9dbcbb8e24b4345cc9972255d2c9
jakhongirs <j.sagdullaev31@gmail.com>
2021-05-05 13:05:53 +0500

    Almost finished categories and products section

--- src/components/Categories/Categotires.js
+++ src/components/Categories/Categotires.js
@@ -46,11 +46,85 @@ function Categories() {
                                 required
                             />
                         </div>
+
+                        <button className="categories__button">Submit</button>
                     </form>
                 </div>
              
                     <div className="categories-result__wrapper">
                         <h2 className="categories__heading">Mavjud Kategoriyalar</h2>
+
+                        <table className='orders__table'>
+                            <thead>
+                                <tr className='orders__table-row'>
+                                    <th className='orders__table-column'>Kategoriya nomi</th>
+                                    <th className='orders__table-column'>Narxi</th>
+                                    <th className='orders__table-column'>O'chirish</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                    <tr className="orders__table-row">
+                                        <td>Futbolka</td>
+                                        <td>65000</td>
+                                        <td>
+                                            <button className="categories__button categories__delete-button">Delete</button>
+                                        </td>
+                                    </tr>
+                            </tbody>
+
+                        </table>
+                    </div>
+                </div>
+             
+                
+                <div className="products__wrapper">
+                    <div className="products__input-wrapper">
+                        <h2 className="categories__heading">Mahsulotlar</h2>
+
+                        <form action="/" method="POST">
+                            <select className="products__select" name="category_id" id="category_id" required>
+                                <option value="" disabled selected>Kategoriya turi</option>
+                                <option value="">Fultbolkalar</option>
+                            </select>
+
+                            <select className="size__select" name="product_size " id="product_size" required>
+                                <option value="" disabled selected>O'lchami</option>
+                                <option value="xs">XS</option>
+                                <option value="s">S</option>
+                                <option value="m">M</option>
+                                <option value="l">L</option>
+                                <option value="xl">XL</option>
+                                <option value="xxl">XXL</option>
+                            </select>
+                            
+                            <button className="categories__button">Submit</button>
+                        </form>
+                    </div>
+                
+                    <div className="products__result">
+                        <h2 className="categories__heading">Mavjud Mahsulotlar</h2>
+
+                        <table className='orders__table'>
+                            <thead>
+                                <tr className='orders__table-row'>
+                                    <th className='orders__table-column'>Kategoriya turi</th>
+                                    <th className='orders__table-column'>O'lchami</th>
+                                    <th className='orders__table-column'>O'chirish</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                    <tr className="orders__table-row">
+                                        <td>Futbolkalar</td>
+                                        <td>M</td>
+                                        <td>
+                                            <button className="categories__button categories__delete-button">Delete</button>
+                                        </td>
+                                    </tr>
+                            </tbody>
+
+                        </table>
                     </div>
                 </div>
              </Container>
