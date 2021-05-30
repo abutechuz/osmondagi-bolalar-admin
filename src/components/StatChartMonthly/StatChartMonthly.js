309663051ef77c2304bbba7af4d1cc7a826dea1b
inspirationjon <ilhomshamidinov55@gmail.com>
2021-05-30 23:03:12 +0500

    e

--- src/components/StatChartMonthly/StatChartMonthly.js
+++ src/components/StatChartMonthly/StatChartMonthly.js
@@ -7,103 +7,103 @@ import moment from 'moment'
 import './StatChartMonthly.scss'
 
 function StatChartMonthly() {
-    const { pathname } = useLocation()
+	const { pathname } = useLocation()
 
-    let dateMonth = moment(new Date()).format('M')
-    let dateYear = new Date().getFullYear()
+	let dateMonth = moment(new Date()).format('M')
+	let dateYear = new Date().getFullYear()
 
-    const [month, setMonth] = React.useState(dateMonth)
-    const [year, setYear] = React.useState(dateYear)
+	const [month, setMonth] = React.useState(dateMonth)
+	const [year, setYear] = React.useState(dateYear)
 
-    const years = [dateYear - 3, dateYear - 1, dateYear]
-    const months = [
-        { stat: 0, monthnumber: 1, month_sm: 'Jan', month_bg: 'January' },
-        { stat: 0, monthnumber: 2, month_sm: 'Feb', month_bg: 'February' },
-        { stat: 0, monthnumber: 3, month_sm: 'Mar', month_bg: 'March' },
-        { stat: 0, monthnumber: 4, month_sm: 'Apr', month_bg: 'April' },
-        { stat: 0, monthnumber: 5, month_sm: 'May', month_bg: 'May' },
-        { stat: 0, monthnumber: 6, month_sm: 'Jun', month_bg: 'June' },
-        { stat: 0, monthnumber: 7, month_sm: 'Jul', month_bg: 'July' },
-        { stat: 0, monthnumber: 8, month_sm: 'Aug', month_bg: 'August' },
-        { stat: 0, monthnumber: 9, month_sm: 'Sep', month_bg: 'September' },
-        { stat: 0, monthnumber: 10, month_sm: 'Oct', month_bg: 'October' },
-        { stat: 0, monthnumber: 11, month_sm: 'Nov', month_bg: 'November' },
-        { stat: 0, monthnumber: 12, month_sm: 'Dec', month_bg: 'December' },
-    ]
+	const years = [dateYear - 3, dateYear - 1, dateYear]
+	const months = [
+		{ stat: 0, monthnumber: 1, month_sm: 'Jan', month_bg: 'January' },
+		{ stat: 0, monthnumber: 2, month_sm: 'Feb', month_bg: 'February' },
+		{ stat: 0, monthnumber: 3, month_sm: 'Mar', month_bg: 'March' },
+		{ stat: 0, monthnumber: 4, month_sm: 'Apr', month_bg: 'April' },
+		{ stat: 0, monthnumber: 5, month_sm: 'May', month_bg: 'May' },
+		{ stat: 0, monthnumber: 6, month_sm: 'Jun', month_bg: 'June' },
+		{ stat: 0, monthnumber: 7, month_sm: 'Jul', month_bg: 'July' },
+		{ stat: 0, monthnumber: 8, month_sm: 'Aug', month_bg: 'August' },
+		{ stat: 0, monthnumber: 9, month_sm: 'Sep', month_bg: 'September' },
+		{ stat: 0, monthnumber: 10, month_sm: 'Oct', month_bg: 'October' },
+		{ stat: 0, monthnumber: 11, month_sm: 'Nov', month_bg: 'November' },
+		{ stat: 0, monthnumber: 12, month_sm: 'Dec', month_bg: 'December' },
+	]
 
-    function findStatus(pathname) {
-        if (pathname === '/chart-stat/monthly-stat') {
-            return 'month/' + year + '/'
-        } else {
-            return ''
-        }
-    }
+	function findStatus(pathname) {
+		if (pathname === '/chart-stat/monthly-stat') {
+			return 'month/' + year + '/'
+		} else {
+			return ''
+		}
+	}
 
-    const fetchProjects = (month) => {
-        return client(findStatus(pathname) + month)
-    }
+	const fetchProjects = (month) => {
+		return client(findStatus(pathname) + month)
+	}
 
-    const { data: graph } = useQuery(
-        ['chart-stat-month', month],
-        () => fetchProjects(month),
-        { keepPreviousData: true }
-    )
+	const { data: graph } = useQuery(
+		['chart-stat-month', month],
+		() => fetchProjects(month),
+		{ keepPreviousData: true },
+	)
 
-    function handleChange(evt) {
-        console.log(evt.target.value)
-        setMonth(evt.target.value)
-    }
+	function handleChange(evt) {
+		console.log(evt.target.value)
+		setMonth(evt.target.value)
+	}
 
-    function handleYearChange(evt) {
-        console.log(evt.target.value)
-        setYear(evt.target.value)
-    }
+	function handleYearChange(evt) {
+		console.log(evt.target.value)
+		setYear(evt.target.value)
+	}
 
-    let labelData = graph && graph.map((item) => item.day)
-    let datasetData = graph && graph.map((item) => item.number)
+	let labelData = graph && graph.map((item) => item.day)
+	let datasetData = graph && graph.map((item) => item.number)
 
-    const data = {
-        labels: labelData,
-        datasets: [
-            {
-                label: moment(month).format('MMMM'),
-                fill: false,
-                data: datasetData,
-                borderColor: ['#8fd6e1'],
-                backgroundColor: ['#8fd6e1'],
-                pointBackgroundColor: ['#1597bb'],
-                pointBorderdColor: ['#8fd6e1'],
-            },
-        ],
-    }
-    return (
-        <>
-            <select
-                className='month-select'
-                name='user_month'
-                onClick={handleChange}>
-                <option defaultValue={month}>Select month</option>
-                {months.map((n, i) => (
-                    <option value={n.monthnumber} key={i + n}>
-                        {n.month_bg}
-                    </option>
-                ))}
-            </select>
+	const data = {
+		labels: labelData,
+		datasets: [
+			{
+				label: moment(month).format('MMMM'),
+				fill: false,
+				data: datasetData,
+				borderColor: ['#8fd6e1'],
+				backgroundColor: ['#8fd6e1'],
+				pointBackgroundColor: ['#1597bb'],
+				pointBorderdColor: ['#8fd6e1'],
+			},
+		],
+	}
+	return (
+		<>
+			<select
+				className='month-select'
+				name='user_month'
+				onClick={handleChange}>
+				<option defaultValue={month}>Select month</option>
+				{months.map((n, i) => (
+					<option value={n.monthnumber} key={i + n}>
+						{n.month_bg}
+					</option>
+				))}
+			</select>
 
-            <select
-                className='month-select'
-                name='user_month'
-                onClick={handleYearChange}>
-                <option defaultValue={year}>Select year</option>
-                {years.map((n, i) => (
-                    <option value={n} key={i + n}>
-                        {n}
-                    </option>
-                ))}
-            </select>
-            <Line data={data} width={500} />
-        </>
-    )
+			<select
+				className='month-select'
+				name='user_month'
+				onClick={handleYearChange}>
+				<option defaultValue={year}>Select year</option>
+				{years.map((n, i) => (
+					<option value={n} key={i + n}>
+						{n}
+					</option>
+				))}
+			</select>
+			<Line data={data} width={500} />
+		</>
+	)
 }
 
 export default StatChartMonthly
--- src/components/WebDashIntro/WebDashIntro.js
+++ src/components/WebDashIntro/WebDashIntro.js
@@ -1,214 +1,207 @@
-import "./WebDashIntro.scss";
+import './WebDashIntro.scss'
 import '../Team/Team.scss'
 import Container from '../Container/Container.js'
-import { useRef } from "react";
-import { useQuery } from "react-query";
+import { useRef } from 'react'
+import { useQuery } from 'react-query'
 
 function WebDashIntro() {
-  const form = useRef();
-  // -----
-  const speakerName = useRef();
-  const speakerJob = useRef();
-  const speakerAbout = useRef();
-  // ---------
-  const videoTitle = useRef();
-  const videoTexts = useRef();
-  const videoYtid = useRef();
-  // ----
-  const sliderImg = useRef();
-  const smallImg = useRef();
-  const bigImg = useRef();
+    const form = useRef()
+    // -----
+    const speakerName = useRef()
+    const speakerJob = useRef()
+    const speakerAbout = useRef()
+    // ---------
+    const videoTitle = useRef()
+    const videoTexts = useRef()
+    const videoYtid = useRef()
+    // ----
+    const sliderImg = useRef()
+    const smallImg = useRef()
+    const bigImg = useRef()
 
-  const { data, isSuccess } = useQuery({
-    queryKey: "video",
+    const { data, isSuccess } = useQuery({
+        queryKey: 'video',
 
-    queryFn: () =>
-      fetch("https://api.osmondagibolalar.uz/videos", {
-        token:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMDYkYWxpb3lLRkVVUWd5VzJ2S2MwUXc3dW1vOVZIWjFDa0U1UUFmTkliT0V0Wi9iSUMvRzZLRi4iLCJpYXQiOjE2MjAwMjQzMDZ9.ifbelGdLxQ3Hg_RbtT3RIpAw9V1MGLBpDkrrnh1zh-I',
-      }).then((data) => data.json()),
-  });
+        queryFn: () =>
+            fetch('https://api.osmondagibolalar.uz/videos', {
+                token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMDYkYWxpb3lLRkVVUWd5VzJ2S2MwUXc3dW1vOVZIWjFDa0U1UUFmTkliT0V0Wi9iSUMvRzZLRi4iLCJpYXQiOjE2MjAwMjQzMDZ9.ifbelGdLxQ3Hg_RbtT3RIpAw9V1MGLBpDkrrnh1zh-I',
+            }).then((data) => data.json()),
+    })
 
-  async function added(evt) {
-    evt.preventDefault();
-    const formData = new FormData();
-    formData.append("video_speaker_name", speakerName.current.value);
-    formData.append("video_speaker_job", speakerJob.current.value);
-    formData.append("video_speaker_about", speakerAbout.current.value);
-    // ----- video
-    formData.append("video_title", videoTitle.current.value);
-    formData.append("video_texts", videoTexts.current.value);
-    formData.append("video_ytid", videoYtid.current.value);
-    // ---- imgs
-    formData.append("sliderImage", sliderImg.current.files[0]);
-    formData.append("smallImage", smallImg.current.files[0]);
-    formData.append("bigImage", bigImg.current.files[0]);
+    async function added(evt) {
+        evt.preventDefault()
+        const formData = new FormData()
+        formData.append('video_speaker_name', speakerName.current.value)
+        formData.append('video_speaker_job', speakerJob.current.value)
+        formData.append('video_speaker_about', speakerAbout.current.value)
+        // ----- video
+        formData.append('video_title', videoTitle.current.value)
+        formData.append('video_texts', videoTexts.current.value)
+        formData.append('video_ytid', videoYtid.current.value)
+        // ---- imgs
+        formData.append('sliderImage', sliderImg.current.files[0])
+        formData.append('smallImage', smallImg.current.files[0])
+        formData.append('bigImage', bigImg.current.files[0])
 
-    await fetch("https://api.osmondagibolalar.uz/videos", {
-      method: "POST",
-      body: formData,
-      headers: {
-        token:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMDYkYWxpb3lLRkVVUWd5VzJ2S2MwUXc3dW1vOVZIWjFDa0U1UUFmTkliT0V0Wi9iSUMvRzZLRi4iLCJpYXQiOjE2MjAwMjQzMDZ9.ifbelGdLxQ3Hg_RbtT3RIpAw9V1MGLBpDkrrnh1zh-I',
-      },
-    }).then(() => alert("Added"));
-  }
+        await fetch('https://api.osmondagibolalar.uz/videos', {
+            method: 'POST',
+            body: formData,
+            headers: {
+                token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMDYkYWxpb3lLRkVVUWd5VzJ2S2MwUXc3dW1vOVZIWjFDa0U1UUFmTkliT0V0Wi9iSUMvRzZLRi4iLCJpYXQiOjE2MjAwMjQzMDZ9.ifbelGdLxQ3Hg_RbtT3RIpAw9V1MGLBpDkrrnh1zh-I',
+            },
+        }).then(() => alert('Added'))
+    }
 
-  async function deleted(e) {
-    const formData = new FormData();
-    formData.append("video_id", e.target.dataset.vid);
-    await fetch(`https://api.osmondagibolalar.uz/videos`, {
-      method: "DELETE",
-      body: formData,
-      headers: {
-        token:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMDYkYWxpb3lLRkVVUWd5VzJ2S2MwUXc3dW1vOVZIWjFDa0U1UUFmTkliT0V0Wi9iSUMvRzZLRi4iLCJpYXQiOjE2MjAwMjQzMDZ9.ifbelGdLxQ3Hg_RbtT3RIpAw9V1MGLBpDkrrnh1zh-I',
-      },
-    }).then(() => alert("Deleted"));
-  }
+    async function deleted(e) {
+        const formData = new FormData()
+        formData.append('video_id', e.target.dataset.vid)
+        await fetch(`https://api.osmondagibolalar.uz/videos`, {
+            method: 'DELETE',
+            body: formData,
+            headers: {
+                token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiIkMmEkMDYkYWxpb3lLRkVVUWd5VzJ2S2MwUXc3dW1vOVZIWjFDa0U1UUFmTkliT0V0Wi9iSUMvRzZLRi4iLCJpYXQiOjE2MjAwMjQzMDZ9.ifbelGdLxQ3Hg_RbtT3RIpAw9V1MGLBpDkrrnh1zh-I',
+            },
+        }).then(() => alert('Deleted'))
+    }
 
-  return (
-    <>
-      <Container className="web-intro">
-        <form
-          ref={form}
-          onSubmit={added}
-          className="web-intro__form"
-          action=""
-          method="POST"
-          autoComplete="off"
-        >
-          <div className="web-intro__speaker speaker">
-            <div className="speaker__inner">
-              <input
-                ref={speakerName}
-                className="speaker__input"
-                type="text"
-                name="speaker_name"
-                placeholder="Speaker Ism va Familiyasi "
-                required
-              />
-              <input
-                ref={speakerJob}
-                className="speaker__input"
-                type="text"
-                name="speaker_job"
-                placeholder="Speaker Ish joyi"
-                required
-              />
-            </div>
-            <textarea
-              ref={speakerAbout}
-              className="speaker__texarea"
-              name="spaeker_about"
-              placeholder="Speaker haqida"
-              required
-            ></textarea>
-          </div>
+    return (
+        <>
+            <Container className='web-intro'>
+                <form
+                    ref={form}
+                    onSubmit={added}
+                    className='web-intro__form'
+                    action=''
+                    method='POST'
+                    autoComplete='off'>
+                    <div className='web-intro__speaker speaker'>
+                        <div className='speaker__inner'>
+                            <input
+                                ref={speakerName}
+                                className='speaker__input'
+                                type='text'
+                                name='speaker_name'
+                                placeholder='Speaker Ism va Familiyasi '
+                                required
+                            />
+                            <input
+                                ref={speakerJob}
+                                className='speaker__input'
+                                type='text'
+                                name='speaker_job'
+                                placeholder='Speaker Ish joyi'
+                                required
+                            />
+                        </div>
+                        <textarea
+                            ref={speakerAbout}
+                            className='speaker__texarea'
+                            name='spaeker_about'
+                            placeholder='Speaker haqida'
+                            required></textarea>
+                    </div>
 
-          <div className="web-intro__speaker speaker">
-            <div className="speaker__inner">
-              <textarea
-                ref={videoTitle}
-                className="speaker__input"
-                type="text"
-                name="video_title"
-                placeholder="Video title (<span></span>)"
-                maxLength="100"
-                required
-              />
-              <input
-                ref={videoYtid}
-                className="speaker__input"
-                type="text"
-                name="video_ytid"
-                placeholder="Video YouTube ID"
-                required
-              />
-            </div>
-            <textarea
-              ref={videoTexts}
-              className="speaker__texarea"
-              name="video_text"
-              placeholder="Video text"
-              required
-            ></textarea>
-          </div>
+                    <div className='web-intro__speaker speaker'>
+                        <div className='speaker__inner'>
+                            <textarea
+                                ref={videoTitle}
+                                className='speaker__input'
+                                type='text'
+                                name='video_title'
+                                placeholder='Video title (<span></span>)'
+                                maxLength='100'
+                                required
+                            />
+                            <input
+                                ref={videoYtid}
+                                className='speaker__input'
+                                type='text'
+                                name='video_ytid'
+                                placeholder='Video YouTube ID'
+                                required
+                            />
+                        </div>
+                        <textarea
+                            ref={videoTexts}
+                            className='speaker__texarea'
+                            name='video_text'
+                            placeholder='Video text'
+                            required></textarea>
+                    </div>
 
-          <div className="web-intro__file">
-            <label className="web-intro__label-btn">
-              <span>Slider Image (PNG)</span>
-              <input
-                ref={sliderImg}
-                className="web-intro__file-input visually-hidden"
-                type="file"
-                name="sliderImage"
-                accept="image/png"
-                required
-                onChange={(e) =>
-                  e.target.parentElement.classList.add(
-                    "web-intro__label-btn--active"
-                  )
-                }
-              />
-            </label>
-            <label className="web-intro__label-btn">
-              <span>Small Image</span>
-              <input
-                ref={smallImg}
-                className="web-intro__file-input visually-hidden"
-                type="file"
-                name="smallImage "
-                accept="image/*"
-                required
-                onChange={(e) =>
-                  e.target.parentElement.classList.add(
-                    "web-intro__label-btn--active"
-                  )
-                }
-              />
-            </label>
-            <label className="web-intro__label-btn">
-              <span>Big Image</span>
-              <input
-                ref={bigImg}
-                className="web-intro__file-input visually-hidden"
-                type="file"
-                name="bigImage "
-                accept="image/*"
-                required
-                onChange={(e) =>
-                  e.target.parentElement.classList.add(
-                    "web-intro__label-btn--active"
-                  )
-                }
-              />
-            </label>
-          </div>
+                    <div className='web-intro__file'>
+                        <label className='web-intro__label-btn'>
+                            <span>Slider Image (PNG)</span>
+                            <input
+                                ref={sliderImg}
+                                className='web-intro__file-input visually-hidden'
+                                type='file'
+                                name='sliderImage'
+                                accept='image/png'
+                                required
+                                onChange={(e) =>
+                                    e.target.parentElement.classList.add(
+                                        'web-intro__label-btn--active'
+                                    )
+                                }
+                            />
+                        </label>
+                        <label className='web-intro__label-btn'>
+                            <span>Small Image</span>
+                            <input
+                                ref={smallImg}
+                                className='web-intro__file-input visually-hidden'
+                                type='file'
+                                name='smallImage '
+                                accept='image/*'
+                                required
+                                onChange={(e) =>
+                                    e.target.parentElement.classList.add(
+                                        'web-intro__label-btn--active'
+                                    )
+                                }
+                            />
+                        </label>
+                        <label className='web-intro__label-btn'>
+                            <span>Big Image</span>
+                            <input
+                                ref={bigImg}
+                                className='web-intro__file-input visually-hidden'
+                                type='file'
+                                name='bigImage '
+                                accept='image/*'
+                                required
+                                onChange={(e) =>
+                                    e.target.parentElement.classList.add(
+                                        'web-intro__label-btn--active'
+                                    )
+                                }
+                            />
+                        </label>
+                    </div>
 
-          <button className="web-intro__btn" type="submit">
-            Send
-          </button>
-        </form>
+                    <button className='web-intro__btn' type='submit'>
+                        Send
+                    </button>
+                </form>
 
-        <ul className="team-members__list web-intro__list">
-          {isSuccess &&
-            data?.map((e) => (
-              <li className="team-members__item" key={e.video_id}>
-                {e.video_speaker_name}
-                <button
-                  className="team-members__item-btn"
-                  data-vid={e.video_id}
-                  onDoubleClick={deleted}
-                >
-                  Delete
-                </button>
-              </li>
-            ))}
-        </ul>
-      </Container>
-    </>
-  );
+                <ul className='team-members__list web-intro__list'>
+                    {isSuccess &&
+                        data?.map((e) => (
+                            <li className='team-members__item' key={e.video_id}>
+                                {e.video_speaker_name}
+                                <button
+                                    className='team-members__item-btn'
+                                    data-vid={e.video_id}
+                                    onDoubleClick={deleted}>
+                                    Delete
+                                </button>
+                            </li>
+                        ))}
+                </ul>
+            </Container>
+        </>
+    )
 }
 
-export default WebDashIntro;
+export default WebDashIntro
